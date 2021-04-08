bce9da9b94aa3eb7b03010a2aa2a876b57729890
naberna <bernaa13@gmail.com>
2021-04-09 00:50:12 +0300

    style of carousel

--- src/components/SimpleCarousel.js
+++ src/components/SimpleCarousel.js
@@ -8,6 +8,17 @@ const Styles = styled.div`
   padding: 40px 0; 
   background: #fff;
   
+  .rec-arrow-right:hover:enabled,
+  .rec-arrow-left:hover:enabled,
+  .rec-arrow-right:focus:enabled,
+  .rec-arrow-left:focus:enabled {
+    background-color: #f6b7b8;
+  }
+
+  .rec-dot_active {
+    background-color: rgba(246,183,184,.5);
+    box-shadow: 0 0 1px 3px rgb(246,183,184);
+  }
   
 `;
 export const SimpleCarousel = () => (
